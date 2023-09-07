b7cb273d27fb734990e50a20a301d05695813fad
Eric Takemoto <24865872+octothorped@users.noreply.github.com>
2023-09-07 13:20:23 -0700

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,7 +1,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
+brew install --cask git-credential-manager
+git credential-manager configure
 brew install exa
 brew install jq
-brew install derailed/k9s/k9s
 brew install powerlevel10k
 echo "source $HOMEBREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+brew install derailed/k9s/k9s
+brew install grpcurl
